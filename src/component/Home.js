659f5e50e8771e404460c3b00c3c711199b63a9f
akhilgangidi <arg170030@utdallas.edu>
2021-09-28 19:38:43 -0500

    Initialized react project

---
+++ src/component/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react'
+
+function Home() {
+    return (
+        <div>
+            <Container>
+                <div>This is the home page!</div>
+            </Container>
+        </div>
+    )
+}
+
+export default Home;
